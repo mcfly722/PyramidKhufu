71a19e20a3105ee58ff7f98a4a31720946fb30bb
Anonymous <7221798@gmail.com>
2020-09-22 00:57:22 +0300

    gallery ceiling added

--- pyramid.cpp
+++ pyramid.cpp
@@ -9,25 +9,27 @@
 #include <cmath>  
 
 #define PI 3.14159265f
-#define c 299792458                                   // Speed of light in vacuum
-#define WIDTH (c / (PI*(sqrt(2)-1)))/1000000          // Pyramid width  = 230,380923883861
-#define HEIGHT WIDTH * 14 / 22                        // Pyramid height = 146,606042471548
+#define c 299792458                                                          // Speed of light in vacuum
+#define WIDTH (c / (PI*(sqrt(2)-1)))/1000000                                 // Pyramid width  = 230,380923883861
+#define HEIGHT WIDTH * 14 / 22                                               // Pyramid height = 146,606042471548
 
 constexpr auto cubit = PI / 6;
 
-#define SIDE sqrt(WIDTH*WIDTH+HEIGHT*HEIGHT)
-#define VERTICAL_ANGLE atan(WIDTH/(2*HEIGHT))         // from top to side angle
+#define SIDE                               sqrt(WIDTH*WIDTH+HEIGHT*HEIGHT)   // pyramid side
+#define VERTICAL_ANGLE                     atan(WIDTH/(2*HEIGHT))            // from top to side angle
 
-#define KINGS_CHAMBER_LEVEL HEIGHT*(1-1/sqrt(2))
+#define KINGS_CHAMBER_LEVEL                HEIGHT*(1-1/sqrt(2))
 
-#define QUEEN_CHAMBER_ROOF_ANGLE      PI / 6
-#define QUEEN_CHAMBER_HEIGHT          6.26f
-#define QUEEN_CHAMBER_WIDTH           10 * cubit
-#define QUEEN_CHAMBER_CENTER_LEVEL    (0.88f+0.83f)
-#define QUEEN_CHAMBER_SMALLEST_BORDER 0.03f
+#define QUEEN_CHAMBER_ROOF_ANGLE           PI / 6
+#define QUEEN_CHAMBER_HEIGHT               6.26f
+#define QUEEN_CHAMBER_WIDTH                10 * cubit
+#define QUEEN_CHAMBER_CENTER_LEVEL         (0.88f+0.83f)
+#define QUEEN_CHAMBER_SMALLEST_BORDER      0.03f
 
-#define KING_CHAMBER_HEIGHT           10 * cubit
-#define KING_CHAMBER_WIDTH            10 * sqrt(5) * cubit / 2
+#define KING_CHAMBER_HEIGHT                10 * cubit
+#define KING_CHAMBER_WIDTH                 10 * sqrt(5) * cubit / 2
+
+#define GALLERY_CEILING_FIRST_STEP_WIDTH   0.37f
 
 constexpr auto shem = 6 * cubit / 5;
 
@@ -39,9 +41,7 @@ constexpr auto angle0 = 0.470322600181172;      //26°56'51"  = 0.470322600181172
 
 constexpr auto defaultAscendingAngle = 0.470322600181172;      //26°56'51"  = 0.470322600181172
 constexpr auto defaultDescendingAngle = 0.46157171323714485;   //26°26'46"
-constexpr auto defaultAngleRange = 5 * PI / 180;
-
-
+constexpr auto defaultAngleRange = 1 * PI / 180;
 
 b2Vec2 crossPoint(b2Vec2 p1, b2Vec2 p2, b2Vec2 p3, b2Vec2 p4) {
 	return b2Vec2(
@@ -60,6 +60,7 @@ public:
 
 	float ReportFixture(b2Fixture* fixture, const b2Vec2& point, const b2Vec2& normal, float fraction) override
 	{
+		m_fixture = fixture;
 		b2Body* body = fixture->GetBody();
 		void* userData = body->GetUserData();
 		if (userData)
@@ -86,6 +87,7 @@ public:
 	bool m_hit;
 	b2Vec2 m_point;
 	b2Vec2 m_normal;
+	b2Fixture* m_fixture;
 };
 
 class NextTo {
@@ -124,28 +126,76 @@ public:
 		ImGui::SetNextWindowSize(ImVec2(700.0f, 400.0f));
 		ImGui::Begin("Controls", nullptr, ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
 
-		//ImGui::ShowDemoWindow();
+		ImGui::ShowDemoWindow();
 
-		if (ImGui::SliderAngle("Ascending Angle  ", &ascendingAngle, (defaultAscendingAngle - defaultAngleRange) * 180 / PI, (defaultAscendingAngle + defaultAngleRange) * 180 / PI, "%0f deg")) {
-			needToReset = true;
+		if (ImGui::TreeNode("Corridors"))
+		{
+			if (ImGui::SliderAngle("Ascending Angle  ", &ascendingAngle, (defaultAscendingAngle - defaultAngleRange) * 180 / PI, (defaultAscendingAngle + defaultAngleRange) * 180 / PI, "%0f deg")) {
+				needToReset = true;
+			}
+
+			if (ImGui::SliderAngle("Descending Angle", &descendingAngle, (defaultDescendingAngle - defaultAngleRange) * 180 / PI, (defaultDescendingAngle + defaultAngleRange) * 180 / PI, "%0f deg")) {
+				needToReset = true;
+			}
+
+			ImGui::TreePop();
 		}
 
-		if (ImGui::SliderAngle("Descending Angle", &descendingAngle, (defaultDescendingAngle - defaultAngleRange) * 180 / PI, (defaultDescendingAngle + defaultAngleRange) * 180 / PI, "%0f deg")) {
-			needToReset = true;
+		if (ImGui::TreeNode("Bottom Chamber"))
+		{
+			if (ImGui::Checkbox("Enable Input Ray", &enableInputRay)) {
+				needToReset = true;
+			}
+
+			ImGui::TreePop();
 		}
 
-		if (ImGui::Checkbox("Enable Input Ray", &enableInputRay)) {
-			needToReset = true;
+		if (ImGui::TreeNode("Queen Chamber"))
+		{
+			if (ImGui::Checkbox("Enable Ray", &enableQueenRay)) {
+				needToReset = true;
+			}
+
+			if (ImGui::SliderAngle("Ray Angle", &queenAngle, 30, 180 - 30, "%0f deg")) {
+				needToReset = true;
+			}
+
+			ImGui::TreePop();
 		}
 
-		ImGui::Separator();
+		if (ImGui::TreeNodeEx("Gallery"))
+		{
+			if (ImGui::SliderFloat("Ceiling Vertical Offset", &galleryCeilingOffset, 0.0f, 12.0f, "%.3f")) {
+				needToReset = true;
+			}
 
-		if (ImGui::Checkbox("Enable Queen Ray", &enableQueenRay)) {
-			needToReset = true;
-		}
+			// Gallery Left Wall Mode ratio group
+			{
+				if (ImGui::RadioButton("Left Wall levels is Horizontal", &leftGalleryWallMode,Horizontal)) {
+					leftGalleryWallMode = Horizontal;
+					needToReset = true;
+				}
+				ImGui::SameLine();
+				if (ImGui::RadioButton("Left Wall levels is Parallel", &leftGalleryWallMode,Parallel)) {
+					leftGalleryWallMode = Parallel;
+					needToReset = true;
+				}
+			}
+			{
+				if (ImGui::RadioButton("Right Wall levels is Horizontal", &rightGalleryWallMode, Horizontal)) {
+					rightGalleryWallMode = Horizontal;
+					needToReset = true;
+				}
+				ImGui::SameLine();
+				if (ImGui::RadioButton("Right Wall levels is Parallel", &rightGalleryWallMode, Parallel)) {
+					rightGalleryWallMode = Parallel;
+					needToReset = true;
+				}
+			}
 
-		if (ImGui::SliderAngle("Queen Ray Angle", &queenAngle, 30, 180-30, "%0f deg")) {
-			needToReset = true;
+
+
+			ImGui::TreePop();
 		}
 
 		ImGui::End();
@@ -200,9 +250,9 @@ public:
 
 		// pyramid
 		g_debugDraw.DrawSegment(p[0] + b2Vec2(-WIDTH / 2, 0), p[0] + b2Vec2(WIDTH / 2, 0), b2Color(1, 1, 1));           // ground base
-		g_debugDraw.DrawSegment(p[0] + b2Vec2(0, HEIGHT), p[0] + b2Vec2(0, 0), b2Color(0.5f, 0.5f, 0.5f));  // vertical
-		g_debugDraw.DrawSegment(p[0] + b2Vec2(-WIDTH / 2, 0), p[0] + b2Vec2(0, HEIGHT), b2Color(1, 1, 1));           // left side
-		g_debugDraw.DrawSegment(p[0] + b2Vec2(WIDTH / 2, 0), p[0] + b2Vec2(0, HEIGHT), b2Color(1, 1, 1));           // right side
+		g_debugDraw.DrawSegment(p[0] + b2Vec2(0, HEIGHT), p[0] + b2Vec2(0, 0), b2Color(0.5f, 0.5f, 0.5f));              // vertical
+		g_debugDraw.DrawSegment(p[0] + b2Vec2(-WIDTH / 2, 0), p[0] + b2Vec2(0, HEIGHT), b2Color(1, 1, 1));              // left side
+		g_debugDraw.DrawSegment(p[0] + b2Vec2(WIDTH / 2, 0), p[0] + b2Vec2(0, HEIGHT), b2Color(1, 1, 1));               // right side
 			   
 		// cross
 		g_debugDraw.DrawSegment(p[15], p[8], b2Color(0, 0, 0.5f));
@@ -274,6 +324,27 @@ public:
 
 		}
 
+		// gallery ceiling
+		{
+			g_debugDraw.DrawSegment(p[52], p[84], b2Color(0, 0, 0.5f));
+			g_debugDraw.DrawSegment(p[84], p[54], b2Color(0, 0, 0.5f));
+			g_debugDraw.DrawSegment(p[54], p[85], b2Color(0, 0, 0.5f));
+			g_debugDraw.DrawSegment(p[85], p[52], b2Color(0, 0, 0.5f));
+
+			float numberOfSteps = ceil(((sqrt((p[52] - p[85]).LengthSquared()) - GALLERY_CEILING_FIRST_STEP_WIDTH) / 1.2f));
+			float angle = atan2(p[52].y-p[85].y,p[85].x-p[52].x);
+			float stepHight = sqrt((p[84] - p[52]).LengthSquared()) / numberOfSteps;
+
+			for (int i = 0;i < numberOfSteps;i++) {
+				b2Vec2 p1 = p[52] + (GALLERY_CEILING_FIRST_STEP_WIDTH + 1.2f * i) * b2Vec2(cos(angle), -sin(angle));
+				b2Vec2 p2 = p[84] + (GALLERY_CEILING_FIRST_STEP_WIDTH + 1.2f * i) * b2Vec2(cos(angle), -sin(angle));
+				g_debugDraw.DrawSegment(p1, p2, b2Color(0, 0, 0.5f));
+				b2Vec2 p3 = p[52] + (stepHight * i) * b2Vec2(sin(angle), cos(angle));
+				b2Vec2 p4 = p[85] + (stepHight * i) * b2Vec2(sin(angle), cos(angle));
+				g_debugDraw.DrawSegment(p3, p4, b2Color(0, 0, 0.5f));
+			}
+		}
+
 		Test::Step(settings);
 	}
 
@@ -283,21 +354,31 @@ public:
 	}
 
 private:
+	enum GalleryWallsMode
+	{
+		Horizontal,
+		Parallel
+	};
 
+	// control parameters 
 	float ascendingAngle = defaultAscendingAngle;
 	float descendingAngle = defaultDescendingAngle;
 	bool enableInputRay = false;
-	
-	bool enableQueenRay = false;
-	float queenAngle = PI/6;
 
+	bool enableQueenRay = false;
+	float queenAngle = PI / 6;
+
+	float galleryCeilingOffset = 1;
+	int leftGalleryWallMode  = Parallel;
+	int rightGalleryWallMode = Parallel;
 
 	b2BodyDef bd;
 	b2Body* pyramidBody;
 
+
 	bool needToReset = false;
 
-	b2Vec2 p[84];
+	b2Vec2 p[89];
 
 	void buildPyramid(b2Body* body) {
 		
@@ -361,7 +442,6 @@ private:
 		p[51] = crossPoint(p[24],p[23], b2Vec2(p[50].x + 0.55f,p[50].y), b2Vec2(p[50].x + 0.55f,p[50].y-10));
 		p[52] = p[51] + b2Vec2(0, 8.74f);
 
-
 		// http://thegreatpyramidofgiza.ca/@Giza$Grand%20Gallery$Chapter_files/image003.jpg
 		float mul1 = 4.22f / 166.2f;
 
@@ -413,6 +493,12 @@ private:
 			p[83] = p[51] + b2Vec2(0, galleryWallsVertical[6]);
 		}
 
+		// gallery ceiling
+		{
+			float ceilingAngle = atan2(p[54].x - p[52].x, p[52].y - p[54].y) - asin(galleryCeilingOffset / sqrt((p[54] - p[52]).LengthSquared()));
+			p[84] = p[52] + galleryCeilingOffset * b2Vec2(cos(ceilingAngle), sin(ceilingAngle));
+			p[85] = p[54] - galleryCeilingOffset * b2Vec2(cos(ceilingAngle), sin(ceilingAngle));
+		}
 
 		drawLine(body, p[8], p[11]);
 		drawLine(body, p[10], p[12]);
@@ -426,25 +512,32 @@ private:
 
 		// Lower Chamber
 		{
-			NextTo LowerChamber_13[7]{
-				b2Vec2(-border0_bottom * sin(descendingAngle),border0_bottom * cos(descendingAngle)),
-				b2Vec2(-8.91 - 8.28,0),
-				b2Vec2(0,2.19 + 0.91),
-				b2Vec2(8.36f,0),
-				b2Vec2(0,-2.19f),
-				b2Vec2(8.78 - 7.39,0),
-				b2Vec2(0, 0)
-			};
-
-			drawPath(body, p[13], LowerChamber_13);
-
 			NextTo LowerChamber_14[3]{
 				b2Vec2(border0_top * sin(angle0),-border0_top * cos(angle0)),
 				b2Vec2(-(8.27f - 3.21f),0),
 				b2Vec2(0, 0)
 			};
 
-			drawPath(body, p[14], LowerChamber_14);
+			p[86] = drawPath(body, p[14], LowerChamber_14);
+
+			NextTo LowerChamber_13[3]{
+				b2Vec2(-border0_bottom * sin(descendingAngle),border0_bottom * cos(descendingAngle)),
+				b2Vec2(-8.91 - 8.28,0),
+				b2Vec2(0, 0)
+			};
+			p[88] = drawPath(body, p[13], LowerChamber_13);
+
+			NextTo LowerChamber_88[5]{
+				b2Vec2(0,2.19f  + 0.91f),
+				b2Vec2(8.36f,0),
+				b2Vec2(0,-2.19f),
+				b2Vec2(8.78 - 7.39,0),
+				b2Vec2(0, 0)
+			};
+
+			p[87] = drawPath(body, p[88], LowerChamber_88);
+
+			drawAbsorbContainer(body, p[86], p[87]);
 		}
 
 
@@ -460,65 +553,100 @@ private:
 			drawPath(body, p[16], QueenChamber_p16);
 			drawLine(body, p[25], p[40]);
 			drawLine(body, p[40],p[40]+ b2Vec2(0, -(p[25].y - p[32].y)));
-
 			drawLine(body, p[39], p[42]);
-
+			drawAbsorbContainer(body, p[44], p[43]);
+			drawAbsorbContainer(body, p[38], p[39]);
+			drawAbsorbContainer(body, p[42], b2Vec2(p[40].x,p[42].y));
 		}
 
 		// Gallery
 		{
 			// Right gallery wall
 			{
-				drawLine(body, p[21], b2Vec2(p[21].x, p[63].y));
-				drawLine(body, b2Vec2(p[21].x, p[63].y), p[63]);
+				float angle = 0;  // Horizontal
 
-				drawLine(body, p[63], b2Vec2(p[63].x, p[64].y));
-				drawLine(body, b2Vec2(p[63].x, p[64].y), p[64]);
+				if (rightGalleryWallMode == Parallel) {
+					angle = ascendingAngle;
+				}
 
-				drawLine(body, p[64], b2Vec2(p[64].x, p[65].y));
-				drawLine(body, b2Vec2(p[64].x, p[65].y), p[65]);
+				drawLine(body, p[21], b2Vec2(p[21].x, p[63].y - (p[21].x - p[63].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[21].x, p[63].y - (p[21].x - p[63].x) * tan(angle)), p[63]);
 
-				drawLine(body, p[65], b2Vec2(p[65].x, p[66].y));
-				drawLine(body, b2Vec2(p[65].x, p[66].y), p[66]);
+				drawLine(body, p[63], b2Vec2(p[63].x, p[64].y - (p[63].x-p[64].x)*tan(angle)));
+				drawLine(body, b2Vec2(p[63].x, p[64].y - (p[63].x - p[64].x) * tan(angle)), p[64]);
 
-				drawLine(body, p[66], b2Vec2(p[66].x, p[67].y));
-				drawLine(body, b2Vec2(p[66].x, p[67].y), p[67]);
+				drawLine(body, p[64], b2Vec2(p[64].x, p[65].y - (p[64].x-p[65].x)*tan(angle)));
+				drawLine(body, b2Vec2(p[64].x, p[65].y - (p[64].x - p[65].x) * tan(angle)), p[65]);
 
-				drawLine(body, p[67], b2Vec2(p[67].x, p[68].y));
-				drawLine(body, b2Vec2(p[67].x, p[68].y), b2Vec2(p[61].x,p[68].y));
-				
+				drawLine(body, p[65], b2Vec2(p[65].x, p[66].y - (p[65].x-p[66].x)*tan(angle)));
+				drawLine(body, b2Vec2(p[65].x, p[66].y - (p[65].x - p[66].x) * tan(angle)), p[66]);
+
+				drawLine(body, p[66], b2Vec2(p[66].x, p[67].y - (p[66].x-p[67].x)*tan(angle)));
+				drawLine(body, b2Vec2(p[66].x, p[67].y - (p[66].x - p[67].x) * tan(angle)), p[67]);
+
+				drawLine(body, p[67], b2Vec2(p[67].x, p[68].y - (p[67].x - p[68].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[67].x, p[68].y - (p[67].x - p[68].x) * tan(angle)), b2Vec2(p[61].x, p[68].y));
+
 				drawLine(body, p[68], p[54]);
 			}
 
 			// Left gallery wall
 			{
-				drawLine(body, p[50], b2Vec2(p[50].x, p[77].y));
-				drawLine(body, b2Vec2(p[50].x, p[77].y), p[77]);
+				float angle = 0;  // Horizontal
 
-				drawLine(body, p[77], b2Vec2(p[77].x, p[78].y));
-				drawLine(body, b2Vec2(p[77].x, p[78].y), p[78]);
+				if (leftGalleryWallMode == Parallel) {
+					angle = ascendingAngle;
+				}
 
-				drawLine(body, p[78], b2Vec2(p[78].x, p[79].y));
-				drawLine(body, b2Vec2(p[78].x, p[79].y), p[79]);
+				drawLine(body, p[50], b2Vec2(p[50].x, p[77].y+ (p[77].x-p[50].x)*tan(angle)));
+				drawLine(body, b2Vec2(p[50].x, p[77].y + (p[77].x - p[50].x) * tan(angle)), p[77]);
 
-				drawLine(body, p[79], b2Vec2(p[79].x, p[80].y));
-				drawLine(body, b2Vec2(p[79].x, p[80].y), p[80]);
+				drawLine(body, p[77], b2Vec2(p[77].x, p[78].y + (p[78].x - p[77].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[77].x, p[78].y + (p[78].x - p[77].x) * tan(angle)), p[78]);
 
-				drawLine(body, p[80], b2Vec2(p[80].x, p[81].y));
-				drawLine(body, b2Vec2(p[80].x, p[81].y), p[81]);
+				drawLine(body, p[78], b2Vec2(p[78].x, p[79].y + (p[79].x - p[78].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[78].x, p[79].y + (p[79].x - p[78].x) * tan(angle)), p[79]);
 
-				drawLine(body, p[81], b2Vec2(p[81].x, p[82].y));
-				drawLine(body, b2Vec2(p[81].x, p[82].y), p[82]);
+				drawLine(body, p[79], b2Vec2(p[79].x, p[80].y + (p[80].x - p[79].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[79].x, p[80].y + (p[80].x - p[79].x) * tan(angle)), p[80]);
 
-				drawLine(body, p[82], b2Vec2(p[82].x, p[83].y));
-				drawLine(body, b2Vec2(p[82].x, p[83].y), p[83]);
+				drawLine(body, p[80], b2Vec2(p[80].x, p[81].y + (p[81].x - p[80].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[80].x, p[81].y+ (p[81].x - p[80].x) * tan(angle)), p[81]);
+
+				drawLine(body, p[81], b2Vec2(p[81].x, p[82].y+ (p[82].x - p[81].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[81].x, p[82].y+ (p[82].x - p[81].x) * tan(angle)), p[82]);
+
+				drawLine(body, p[82], b2Vec2(p[82].x, p[83].y+ (p[83].x - p[82].x) * tan(angle)));
+				drawLine(body, b2Vec2(p[82].x, p[83].y+ (p[83].x - p[82].x) * tan(angle)), p[83]);
 
 				drawLine(body, p[83], p[52]);
 			}
 
 			// Gallery ceiling
 			{
-				drawLine(body, p[52], p[54]);
+				if (galleryCeilingOffset == 0) {
+					drawLine(body, p[52], p[54]);
+				}
+				else {
+					float numberOfSteps = ceil(((sqrt((p[52] - p[85]).LengthSquared()) - GALLERY_CEILING_FIRST_STEP_WIDTH) / 1.2f));
+					float angle = atan2(p[52].y - p[85].y, p[85].x - p[52].x);
+					float stepHight = sqrt((p[84] - p[52]).LengthSquared()) / numberOfSteps;
+
+					b2Vec2 previousPoint = p[52];
+
+					for (int i = 0;i < numberOfSteps;i++) {
+						b2Vec2 p1 = p[52] + (GALLERY_CEILING_FIRST_STEP_WIDTH + 1.2f * i) * b2Vec2(cos(angle), -sin(angle));
+						b2Vec2 p2 = p[84] + (GALLERY_CEILING_FIRST_STEP_WIDTH + 1.2f * i) * b2Vec2(cos(angle), -sin(angle));
+						b2Vec2 p3 = p[52] + (stepHight * i) * b2Vec2(sin(angle), cos(angle));
+						b2Vec2 p4 = p[85] + (stepHight * i) * b2Vec2(sin(angle), cos(angle));
+						b2Vec2 p = crossPoint(p1, p2, p3, p4);
+						b2Vec2 p_new = p + stepHight * b2Vec2(sin(angle), cos(angle));
+						drawLine(body, previousPoint, p);
+						drawLine(body, p, p_new);
+						previousPoint = b2Vec2(p_new.x, p_new.y);
+					}
+					drawLine(body, previousPoint, p[54]);
+				}
 			}
 
 
@@ -605,11 +733,12 @@ private:
 
 	}
 
+
+
 	b2Vec2 reflect(b2Vec2 vector, b2Vec2 normal) {
 		float num2 = vector.x * normal.x + vector.y * normal.y;
 		return b2Vec2(vector.x - 2.0f * num2 * normal.x, vector.y - 2.0f * num2 * normal.y);
 	}
-
 	void drawPoint(b2Vec2 point, char* name) {
 		g_debugDraw.DrawCircle(point, 0.1f, b2Color(1, 1, 1));
 		g_debugDraw.DrawString(point, name);
@@ -650,6 +779,10 @@ private:
 
 					b2Vec2 direction = destination - callback.m_point;
 					source = callback.m_point + 0.0001f * b2Vec2(direction.x / direction.Length(), direction.y / direction.Length());
+
+					if (callback.m_fixture->GetUserData() == "absorb") {
+						break;
+					}
 
 				}
 			}
@@ -705,6 +838,28 @@ private:
 		shape.SetTwoSided(startPoint, endPoint);
 		body->CreateFixture(&fd);
 	};
+	void drawAbsorbLine(b2Body* body, b2Vec2 startPoint, b2Vec2 endPoint) {
+		b2EdgeShape shape;
+		b2FixtureDef fd;
+		fd.shape = &shape;
+		fd.density = 0.0f;
+		fd.friction = 0.6f;
+		fd.userData = "absorb";
+		shape.SetTwoSided(startPoint, endPoint);
+		body->CreateFixture(&fd);
+	};
+	void drawAbsorbContainer(b2Body* body, b2Vec2 startPoint, b2Vec2 endPoint) {
+		constexpr auto absorbContainerDepth = 0.4f;
+
+		float angle = atan2(endPoint.y-startPoint.y,endPoint.x-startPoint.x)-PI/2;
+
+		b2Vec2 p1 = startPoint + absorbContainerDepth * b2Vec2(cos(angle), sin(angle));
+		b2Vec2 p2 = endPoint + absorbContainerDepth * b2Vec2(cos(angle), sin(angle));
+
+		drawAbsorbLine(body, startPoint, p1);
+		drawAbsorbLine(body, endPoint  , p2);
+		drawAbsorbLine(body, p1, p2);
+	}
 };
 
 static int testIndex = RegisterTest("Collision", "Piramid", Piramid::Create);
